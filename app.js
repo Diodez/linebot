5e0c52546760b9ab138f7405e6f21acc1ba0c911
Diodez <50066693+Diodez@users.noreply.github.com>
2019-04-28 14:49:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.post('/webhook', (req, res) => {
         msg = "ดีจ้า";
     }
     
-    reply(reply_token, msg)
+    reply(reply_token, msg);
+    reply_2(reply_token, msg);
     res.sendStatus(200)
 });
 
@@ -32,13 +33,32 @@ function reply(reply_token, msg) {
         replyToken: reply_token,
         messages: [
             {
-            type: 'text',
-            text: msg
-            },
-            {
                 type: 'sticker',
                 packageId: '1',
-                stickerId: '1'
+                stickerId: '2565',
+            }
+        ]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
+function reply_2(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 0+X6YrX7OfP83ERqESsjptG6XVZ9XFJvtKWfI0npquj4lN9vG+UcJoVvEQF0muWn6nk74+BKqIKZVLYx2fttTQt8RlT5TASOicCHzx1W3KeT1q5ZwhBFbIWoF7hQOlErJTYN/FRynvINx4RRS7EhUAdB04t89/1O/w1cDnyilFU='
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [
+            {
+                type: 'text',
+                text: msg,
             }
         ]
     })
