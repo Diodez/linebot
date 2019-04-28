9bddb8c21f219c824a98f35455baaa34922e4cdd
Diodez <50066693+Diodez@users.noreply.github.com>
2019-04-28 12:50:25 +0700

    start project
    add lib
    express, request, body-parser

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/webhook', async (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(4000);
